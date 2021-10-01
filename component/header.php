bebc0fab9bcacd51bb65204adb07e72e844dce4e
Ariya Adinatha <13519048@std.stei.itb.ac.id>
2021-10-02 02:11:07 +0700

    dashboard beres

---
+++ component/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Dorayaki AMK </title>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../asset/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <img src="../asset/img/dorayaki.svg">
+                <a href="index.html"> <h2> ApelManggaKucing </h2> </a>
+            </div>
+            <form method="#" action="#" class="form-search">
+                <input type="text" class="navbar-input" placeholder="Search for dorayaki here">
+                <button class="navbar-submit" type="submit"> <img src="../asset/img/search.svg" class="search-img"> </button>
+            </form>
+            <a href="#"> Daftar Pembelian </a>
+            <button class="navbar-button"> username </button>
+        </nav>
+     </header>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include "component/header.php";
+?>
+
+ <!-- Start of Dashboard -->
+<section class="dashboard">
+    <div class="container">
+        <div class="paragraph">
+            <div class="title">
+                <h1> 
+                    LOREM 
+                    <br> IPSUM DOLOR 
+                </h1>
+            </div>
+            <div class="subtitle">
+                <h4> lorem ipsum dolor sit amet </h4>
+            </div>
+            <div class="content">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Dolor saepe necessitatibus quam eligendi suscipit animi 
+                assumenda vel laborum at nam natus quod, fuga beatae 
+                aperiam ad molestiae consequatur soluta voluptas?
+            </div>
+            <a href="#"> <button class="primary-button"> Learn More </button> </a>
+        </div>
+        <div class="flat">
+            <img src="asset/img/doraemon1.png">
+        </div>
+    </div>
+    <div class="arrow-down">
+        <a href="#"> &#8964; </a>
+    </div>
+</section>
+<!-- End of Dashboard -->
+
+<?php
+include "component/footer.php";
+?>
+
