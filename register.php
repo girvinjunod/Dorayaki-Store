dd50544eda4475fed1488b7fffb762bf21741fa7
girvinjunod <girvinjunod@gmail.com>
2021-10-05 00:57:36 +0700

    add frontend validation to register

--- register.php
+++ register.php
@@ -6,11 +6,23 @@
     <link rel="stylesheet" href="assets/global.css">
     <link rel="stylesheet" href="assets/register.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
     <title>Register</title>
 </head>
 <body>
+
+<?php 
+    if ($_GET["err"]==1){ ?>
+    <div class="error-msg">
+        <p>Please fill all the required fields properly before registering.</p>
+    </div>
+
+    <?php
+    }
+?>
+
+
 <div class="container">
     <div class="gambar">
         <img src="assets/img/register.png" alt="doraemon buka pintu" class="doraemon">
@@ -18,7 +30,7 @@
 
     <div class="form-register">
         <h2>Register</h2>
-        <form action="submitRegister.php" method="POST" class="form">
+        <form action="submitRegister.php" method="POST" class="form" onsubmit="return valSubmit();">
             <input type="text" name="email" id="email"
             placeholder="Email" onblur="valEmail(this.value);">
             <label for="email" class="email-err hide">Please enter a valid email.</label>
@@ -35,15 +47,7 @@
             placeholder="Confirm Password" onkeyup="valConfirmPassword(this.value)"> 
             <label for="confirmpassword" class="confirmpass-err hide">Those passwords didn't match, try again.</label>
             
-            <?php 
-            if($_GET["err"]=="1"){
-            ?>
-                <p id="submit-err">Please fill all the required fields properly before registering.</p>
-            <?php 
-            }
-            ?>
-            
-            
+            <p id="submit-err" class="hide">Please fill all the required fields properly before registering.</p>
             <button class="reg-button">Register</button>
         </form>
         
@@ -52,6 +56,24 @@
 </div>
 
 <script>
+    //frontend submit validation
+    function valSubmit(){
+        var email = document.getElementsByName("email")[0].classList.length;
+        var uname = document.getElementsByName("uname")[0].classList.length;
+        var pass = document.getElementsByName("password")[0].classList.length;
+        var confpass = document.getElementsByName("confirmpassword")[0].classList.length;
+        
+        if (!(email && uname && pass && confpass)){
+            event.preventDefault();
+            var submitErr = document.querySelector("#submit-err");
+            submitErr.classList.remove("hide");
+        } else{
+            return true;
+        }
+    }
+
+
+
     function valEmail(email){
         var msg = document.querySelector(".email-err");
         var input = document.getElementsByName("email")[0];
@@ -77,15 +99,6 @@
             msg.classList.add("hide");
             input.classList.remove("correct");
         }
-
-        <?php 
-            if($_GET["err"]=="1"){
-            ?>
-                var submitErr = document.querySelector("#submit-err");
-                submitErr.classList.add("hide");
-            <?php 
-            }
-        ?>
     }
 
     function valUname(uname){
@@ -114,15 +127,6 @@
             msg.classList.add("hide");
             input.classList.remove("correct");
         }
-
-        <?php 
-            if($_GET["err"]=="1"){
-            ?>
-                var submitErr = document.querySelector("#submit-err");
-                submitErr.classList.add("hide");
-            <?php 
-            }
-        ?>
     }
 
     function valPassword(password){
@@ -146,15 +150,6 @@
         } else{
             msg.classList.add("hide");
         }
-
-        <?php 
-            if($_GET["err"]=="1"){
-            ?>
-                var submitErr = document.querySelector("#submit-err");
-                submitErr.classList.add("hide");
-            <?php 
-            }
-        ?>
     }
 
     function valConfirmPassword(conpassword){
@@ -186,15 +181,21 @@
             input1.classList.remove("correct");
             input2.classList.remove("correct");
         }
-        <?php 
-            if($_GET["err"]=="1"){
-            ?>
-                var submitErr = document.querySelector("#submit-err");
-                submitErr.classList.add("hide");
-            <?php 
-            }
-        ?>
     }
+
+    <?php 
+    if ($_GET["err"]==1){ ?>
+    setTimeout( function() {
+        
+        var sign = document.querySelector(".error-msg");
+        sign.classList.add("hide2");
+    } ,
+    5000)
+
+
+    <?php
+    }
+    ?>
 
 
 </script>
