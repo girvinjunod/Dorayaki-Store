9698934325cc1c5d7a80cee400b33effb745504f
Zenovore <agustinus.alexander17@gmail.com>
2021-10-09 21:54:15 +0700

    feat:Searchpage, pagination not done

--- search.php
+++ search.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/global.css">
   <link rel="stylesheet" href="assets/itemlist.css">
+  <link rel="stylesheet" href="assets/search.css">
   <title>Search Result</title>
 </head>
 <body>
@@ -16,14 +17,13 @@ include "component/header.php";
         <div class="card-list">
 <?php
 
-  // if (){
     $db = new SQLite3('db/doraemon.db');
     if (isset($_GET['q'])){
-      $test = $db->prepare("select * from dorayaki where nama LIKE ?");
+      $test = $db->prepare("select * from dorayaki where nama LIKE ? LIMIT 8");
       $query = '%'.$_GET['q'].'%';
       $test->bindParam(1,$query);
     }else{
-      $test = $db->prepare("select * from dorayaki");
+      $test = $db->prepare("select * from dorayaki LIMIT 8 OFFSET 5");
 
     }
     // echo '</br>';
@@ -35,16 +35,22 @@ include "component/header.php";
     while ($cek = $res->fetchArray(SQLITE3_ASSOC)){ 
     ?>
       <div class="card">
-          <img src="https://dummyimage.com/600x600/000/fff">
+          <img src="<?php echo $cek['gambar'] ?>">
           <div class="card-content">
               <div class="title">
                   <h6> <?php echo $cek['nama'] ?> </h6>
               </div>
               <div class="subtitle">
-                  <h6> Rp <?php echo $cek['harga'] ?></h6>
+                  <h6> Rp <?php echo $cek['harga']  ?></h6>
               </div>
               <div class="content">
-              <?php echo $cek['deskripsi'] ?> 
+              <?php  
+              if (strlen($cek['deskripsi']) >= 100){
+                echo substr($cek['deskripsi'],0,100).'...' ;
+              } else{
+                echo $cek['deskripsi'];
+              }
+              ?> 
               </div>
               <button class="primary-button">
                   BUY
@@ -55,7 +61,16 @@ include "component/header.php";
   // }
   ?>
         </div>
-    </div>
+      </div>
+      <div class="pagination justify-content-center">
+        <div class="option">Previous</div>
+        <div class="option">1</div>
+        <div class="option">2</div>
+        <div class="option">3</div>
+        <div class="option">4</div>
+        <div class="option">5</div>
+        <div class="option">Next</div>
+      </div>
 </body>
 </html>
 
