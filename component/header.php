f5c37af41ec361b100270f0a4f70987c8ed0148c
Ariya Adinatha <13519048@std.stei.itb.ac.id>
2021-10-15 20:04:38 +0700

    logic login beres

--- component/header.php
+++ component/header.php
@@ -1,9 +1,16 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
     <title> Dorayaki AMK </title>
     <!-- Custom CSS -->
     <link rel="stylesheet" href="../assets/style.css">
@@ -22,6 +29,12 @@
                 <button class="navbar-submit" type="submit"> <img src="../assets/img/search.svg" class="search-img"> </button>
             </form>
             <a href="#"> Daftar Pembelian </a>
-            <button class="navbar-button"> username </button>
+            <?php
+                if ($_SESSION['loginstate'] == false) {
+                    echo '<a href="login.php"> <button class="navbar-button"> LOGIN </button> </a>';
+                } else {
+                    echo '<button class="navbar-button">'.$_SESSION['username'].'</button>';
+                }
+            ?>
         </nav>
      </header>
--- login.php
+++ login.php
@@ -1,17 +1,13 @@
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/global.css">
-    <link rel="stylesheet" href="assets/register.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/login.css">
     <title>Login</title>
 </head>
 
-<body>
+<?php
+    include "component/header.php";
+?>
+
     <div class="container">
         <div class="gambar">
             <img src="assets/img/register.png" alt="doraemon buka pintu" class="doraemon">
@@ -19,7 +15,7 @@
 
         <div class="form-register">
             <h2>LOGIN</h2>
-            <form action="#" method="POST" class="form">
+            <form action="submitLogin.php" method="POST" class="form">
                 <input type="text" name="email" id="email"
                     placeholder="Email" onblur="valEmail(this.value);">
                 <label id="info-label">
@@ -29,11 +25,12 @@
                 <input type="password" name="password" id="password" 
                     placeholder="Password" onblur="valPassword(this.value);" class="pass">
                 <label id="info-label">
-                    Your password needs to have a minimum of 6 characters.
+                    Don't have an account? <a href="register.php"> click here </a>
                 </label>
-                <button class="reg-button" type="submit">LOGIN</button>
+                <button class="reg-button" type="submit" name="submit">LOGIN</button>
             </form>
         </div>
     </div>
-</body>
-</html>
+<?php
+    include "component/footer.php";
+?>
