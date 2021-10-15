9fb23181a36c0dbadd374cd082d2b357944ba675
Zenovore <agustinus.alexander17@gmail.com>
2021-10-16 04:19:50 +0700

    feat: Search dorayaki and pagination

--- search.php
+++ search.php
@@ -4,73 +4,67 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/global.css">
+  <link rel="stylesheet" href="assets/style.css">
+  <!-- <link rel="stylesheet" href="assets/global.css"> -->
   <link rel="stylesheet" href="assets/itemlist.css">
   <link rel="stylesheet" href="assets/search.css">
   <title>Search Result</title>
 </head>
 <body>
+<script>
+      function load_data(page_number){
+        var form_data = new FormData();
+        query = '<?php 
+        if(isset($_GET['q'])){
+          echo $_GET['q'] ;
+        }
+        ?>';
+        if (query){
+          form_data.append('query', query);
+        }
+        form_data.append('page', page_number);
+        var req = new XMLHttpRequest();
+        req.open('POST','getSearchData.php');
+        req.send(form_data);
+        req.onload = function(){
+          
+          var data = JSON.parse(this.responseText);
+          console.log(data.debug);
+          const dataItem = data.data
+          var input = '';
+          for(var count = 0; count < dataItem.length; count++)
+            {
+              input += '<div class="card">';
+              input += '<img src="'+ dataItem[count]["gambar"]+'">';
+              input += '<div class="card-content">';
+              input += '<div class="title"><h6>'+(dataItem[count]["nama"].slice(0,15) + (dataItem[count]["nama"].length > 15 ? "..." : ""))+'</h6></div>';
+              input += '<div class="subtitle"><h6>Rp. '+dataItem[count]["harga"]+'</h6></div>';
+              input += '<div class="content"><h6>'+(dataItem[count]["deskripsi"].slice(0,100) + (dataItem[count]["deskripsi"].length > 100 ? "..." : ""))+'</h6></div>';
+              input += '<button class="primary-button"> BUY </button>'
+              input += '</div>';
+              input += '</div>';
+            }
+          document.getElementById('itemData').innerHTML = input;
+          document.getElementById('paginationData').innerHTML = data.pagination;
+        }
+      }
+    </script>  
+  <script>
+    load_data(1)
+  </script>
 <?php
 include "component/header.php";
 ?>
-    <div class="container">
-        <div class="card-list">
-<?php
+<div class="card-container">
+      <h1 class="search-result">Search Result</h1>
+        <div class="card-list" id="itemData">
 
-    $db = new SQLite3('db/doraemon.db');
-    if (isset($_GET['q'])){
-      $test = $db->prepare("select * from dorayaki where nama LIKE ? LIMIT 8");
-      $query = '%'.$_GET['q'].'%';
-      $test->bindParam(1,$query);
-    }else{
-      $test = $db->prepare("select * from dorayaki LIMIT 8 OFFSET 5");
-
-    }
-    // echo '</br>';
-    // echo $query;
-    // echo '</br>';
-    $res = $test->execute();
-    
-    
-    while ($cek = $res->fetchArray(SQLITE3_ASSOC)){ 
-    ?>
-      <div class="card">
-          <img src="<?php echo $cek['gambar'] ?>">
-          <div class="card-content">
-              <div class="title">
-                  <h6> <?php echo $cek['nama'] ?> </h6>
-              </div>
-              <div class="subtitle">
-                  <h6> Rp <?php echo $cek['harga']  ?></h6>
-              </div>
-              <div class="content">
-              <?php  
-              if (strlen($cek['deskripsi']) >= 100){
-                echo substr($cek['deskripsi'],0,100).'...' ;
-              } else{
-                echo $cek['deskripsi'];
-              }
-              ?> 
-              </div>
-              <button class="primary-button">
-                  BUY
-              </button>
-          </div>
-      </div>
-    <?php } 
-  // }
-  ?>
         </div>
-      </div>
-      <div class="pagination justify-content-center">
-        <div class="option">Previous</div>
-        <div class="option">1</div>
-        <div class="option">2</div>
-        <div class="option">3</div>
-        <div class="option">4</div>
-        <div class="option">5</div>
-        <div class="option">Next</div>
-      </div>
+      <div class="pagination justify-content-center" id="paginationData">
+
+      </div>        
+</div>
+
 </body>
 </html>
 
