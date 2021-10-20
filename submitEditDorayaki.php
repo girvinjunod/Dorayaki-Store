4602268cb1bd511c75dc875dc3d2395ff2cc16f3
girvinjunod <girvinjunod@gmail.com>
2021-10-20 23:41:09 +0700

    feat/edit-image varian

--- submitEditDorayaki.php
+++ submitEditDorayaki.php
@@ -2,11 +2,21 @@
   session_start();
   if (isset($_POST)){
     $db = new SQLite3('db/doraemon.db');
+    echo "hai" . $_FILES["img"]["name"] . "<br>";
+    $img = getimagesize($_FILES["img"]["tmp_name"]);
+    echo $img;
     $nama = $_POST['nama'];
     $id = $_POST['id'];
-    echo $nama;
     $deskripsi = $_POST['deskripsi'];
     $harga = $_POST['harga'];
+    $img = getimagesize($_FILES["img"]["tmp_name"]);
+    if ($img){
+      $target_dir = "db/img/";
+      $target_file = $target_dir . $id . ".";
+      $imageFileType = strtolower(pathinfo($_FILES["img"]["name"],PATHINFO_EXTENSION));
+      $target_file .= $imageFileType;
+      move_uploaded_file($_FILES["img"]["tmp_name"], $target_file);
+    }
     $queryEditDorayaki = $db->prepare("UPDATE dorayaki SET nama = ?, harga = ?, deskripsi = ? WHERE id=?");
     $queryEditDorayaki->bindParam(1, $nama);
     $queryEditDorayaki->bindParam(2, $harga);
