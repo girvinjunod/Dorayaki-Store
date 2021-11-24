7bc627c39448fe738b3e649111f1c512e8eb0ae6
Zenovore <agustinus.alexander17@gmail.com>
2021-11-24 14:10:28 +0700

    fix: change db path to fullpath

--- cronjob.php
+++ cronjob.php
@@ -26,34 +26,39 @@ function updateDatabase($db,$id_recipe,$change){
   $updateRiwayat = $queryUpdateRiwayat->execute();
   $updateResult = $queryUpdateData->execute();
 }
-
-$db = new SQLite3('db/doraemon.db');
-$soapclient = new SoapClient('http://localhost:8080/webservice/apelmanggakucing/?wsdl');
-$ip_server = $_SERVER['REMOTE_ADDR'];
-$ip_port = $_SERVER['SERVER_PORT'];
-$ip_data = $ip_server.':'.$ip_port;
-$params = array('arg0'=> $ip_data);
-$response = $soapclient->getStatusRequest($params);
-// var_dump($response);
-// print_r($response)
-$is_empty = true;
-$newarray = array();
-foreach ($response as $value) {
-  $newarray = $value;
-  $is_empty = false;
-}
-if (is_array($newarray)){
-  foreach ($newarray as $value) {
-    $seperatedData = explode(';',$value);
-    $id_recipe = $seperatedData[0];
-    $change = $seperatedData[1];
-    updateDatabase($db,$id_recipe ,$change);
-  }  
-} else if (!$is_empty) {
-    $seperatedData = explode(';',$newarray);
-    updateDatabase($db,$id_recipe ,$change);
-} else {
-  return;
+try{
+  $db = new SQLite3('/mnt/d/Kuliah/Semester5/WBD/tubes2/dorayaki-store/db/doraemon.db'); // CHANGE TO FULL PATH
+  $soapclient = new SoapClient('http://localhost:8080/webservice/apelmanggakucing/?wsdl');
+  $ip_server = '127.0.0.1';
+  // $ip_port = $_SERVER['SERVER_PORT'];
+  $ip_data = $ip_server;
+  $params = array('arg0'=> $ip_data);
+  $response = $soapclient->getStatusRequest($params);
+  // var_dump($response);
+  // print_r($response)
+  $is_empty = true;
+  $newarray = array();
+  foreach ($response as $value) {
+    $newarray = $value;
+    $is_empty = false;
+  }
+  if (is_array($newarray)){
+    foreach ($newarray as $value) {
+      $seperatedData = explode(';',$value);
+      $id_recipe = $seperatedData[0];
+      $change = $seperatedData[1];
+      updateDatabase($db,$id_recipe ,$change);
+    }  
+  } else if (!$is_empty) {
+      $seperatedData = explode(';',$newarray);
+      $id_recipe = $seperatedData[0];
+      $change = $seperatedData[1];
+      updateDatabase($db,$id_recipe ,$change);
+  } else {
+    return;
+  }
+} catch (Exception $e){
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 // try{
 //   foreach ($newarray as $value) {
